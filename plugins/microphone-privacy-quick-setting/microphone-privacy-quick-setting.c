d3f5bede8309be413207b2a9e70126760c93bff2
Bardia Moshiri <bardia@furilabs.com>
2025-04-10 14:45:15 -0400

    microphone-privacy-quick-settings: adapt to latest phosh changes
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- plugins/microphone-privacy-quick-setting/microphone-privacy-quick-setting.c
+++ plugins/microphone-privacy-quick-setting/microphone-privacy-quick-setting.c
@@ -66,7 +66,7 @@ on_settings_changed (PhoshMicrophonePrivacyQuickSetting *self,
 }
 
 static void
-phosh_microphone_finalize (GObject *object)
+phosh_microphone_privacy_finalize (GObject *object)
 {
   PhoshMicrophonePrivacyQuickSetting *self = PHOSH_MICROPHONE_PRIVACY_QUICK_SETTING (object);
 
@@ -75,14 +75,13 @@ phosh_microphone_finalize (GObject *object)
   G_OBJECT_CLASS (phosh_microphone_privacy_quick_setting_parent_class)->finalize (object);
 }
 
-
 static void
 phosh_microphone_privacy_quick_setting_class_init (PhoshMicrophonePrivacyQuickSettingClass *klass)
 {
   GObjectClass *object_class = (GObjectClass *)klass;
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
-  object_class->finalize = phosh_microphone_finalize;
+  object_class->finalize = phosh_microphone_privacy_finalize;
 
   gtk_widget_class_set_template_from_resource (widget_class,
                                                "/mobi/phosh/plugins/microphone-privacy-quick-setting/qs.ui");
